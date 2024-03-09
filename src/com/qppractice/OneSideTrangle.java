2eab4e041aee92e0d2bd496dc549afca8edcef20
QED42 <qed42@QED42s-MacBook-Air.local>
2024-03-09 14:43:13 +0530

    Trandle design patter

---
+++ src/com/qppractice/OneSideTrangle.java
@@ -0,0 +1,15 @@
+package com.qppractice;
+
+public class OneSideTrangle {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("#");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
